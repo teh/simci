3bbfc469a37ad732291bef3f4692300c401b1faa
Tom Hunger <tehunger@gmail.com>
2018-10-28 22:42:05 +0000

    Build any repo that can be nix imported.

--- template.nix
+++ template.nix
@@ -1,23 +1,29 @@
-{ attributepath ? ["hello"], tarPath }: with (import (builtins.fetchTarball tarPath) {}).pkgs;
+{ attributepath ? ["hello"], tarPath }:
 let
-contents = lib.getAttrFromPath attributepath pkgs;
-configJson = writeText "layers.json" (builtins.toJSON {
+# host packages to host the mkManyPureLayers
+hostPackages = (import (fetchTarball {
+  url = https://github.com/NixOS/nixpkgs/archive/d9423ee4464e02fd90eb2ca6192017a00a0805bf.tar.gz;
+  sha256 = "1rc8n5vmkzbaj5kf4qwaxii103pi6s27ll1n0dvcgrxiw871dysw";
+}) {});
+tarImport = (import (builtins.fetchTarball tarPath) {});
+contents = hostPackages.lib.getAttrFromPath attributepath tarImport;
+configJson = hostPackages.writeText "layers.json" (builtins.toJSON {
   architecture = "amd64";
   os = "linux";
   config = {};
 });
-layer1 = dockerTools.mkManyPureLayers {
+layer1 = hostPackages.dockerTools.mkManyPureLayers {
       name = "Blarg";
       closure = contents;
       configJson = configJson;
       maxLayers = 100;
     };
-custom = dockerTools.mkCustomisationLayer {
+custom = hostPackages.dockerTools.mkCustomisationLayer {
   name = "blarg2";
   contents = contents;
   baseJson = configJson;
 };
-in writeText "layers.json" (builtins.toJSON {
+in hostPackages.writeText "layers.json" (builtins.toJSON {
   layers = layer1;
   toplayer = custom;
 })
